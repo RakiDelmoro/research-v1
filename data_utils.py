d8b10fcf3ba213d757887f1b6d3ed1f1d64b0880
Raki Delmoro <rakidelmoro@gmail.com>
2026-01-20 13:26:36 +0000

    get batched for vision task

--- data_utils.py
+++ data_utils.py
@@ -4,13 +4,13 @@ import requests
 import numpy as np
 
 def fetch_data():
-    if not os.path.exists('input.txt'):
+    if not os.path.exists('./datasets/input.txt'):
         data_url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
         with open('input.txt', "w") as f:
             f.write(requests.get(data_url).text)
         
-def get_batch(split, batch_size, context_size):
-    data = np.memmap('input.txt', dtype=np.uint8, mode='r')
+def text_get_batch(split, batch_size, context_size):
+    data = np.memmap('./datasets/input.txt', dtype=np.uint8, mode='r')
     if split == 'train':
         data = data[: int(0.9 * len(data))]
     else:
@@ -27,3 +27,17 @@ def get_batch(split, batch_size, context_size):
         x, y = batched_x.to('cpu'), batched_y.to('cpu')
 
     return x, y
+
+def image_get_batch(img_arr, label_arr, batch_size, shuffle):
+    num_samples = img_arr.shape[0]    
+    indices = np.arange(num_samples)
+    if shuffle: np.random.shuffle(indices)
+
+    for start_idx in range(0, num_samples, batch_size):
+        end_idx = min(start_idx + batch_size, num_samples)
+        current_batch_size = end_idx - start_idx
+
+        batched_img = img_arr[indices[start_idx:end_idx]].reshape(current_batch_size, 1, 28, 28)
+        batched_label = label_arr[indices[start_idx:end_idx]]
+
+        yield batched_img, batched_label
