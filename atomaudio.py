9cba45e539867b0b170f5715f9b3123665e64b06
Raki Delmoro <rakidelmoro@gmail.com>
2026-01-22 13:23:11 +0000

    update

--- atomaudio.py
+++ atomaudio.py
@@ -1,25 +1,11 @@
 import torch
 import pickle
 from data_utils import audio_get_batch
+from utils import save_checkpoint
 from node import universal_node
 
-# Training configs
-AUDIO_PATCH_SIZE = 16
-BATCH_SIZE = 32
-MAX_EPOCHS = 3000
-LEARNING_RATE = 1e-3
-
-# Model configs
-REFINEMENT_STEPS = 1
-NUM_EMBED_DIM = 256
-NUM_MINI_POOL = 1
-NEURONS_PER_MINI_POOL = 1024
-DROPOUT = 0.1
-NUM_CLASSES = 10
-
 def audio_to_patch_embeddings(weight, bias=None, patch_size=4, stride=None):
-    if stride is None:
-        stride = patch_size
+    if stride is None: stride = patch_size
 
     def forward_pass(input_batch):
         conv2d_out = torch.nn.functional.conv2d(input_batch, weight, bias, stride=stride)
@@ -30,12 +16,20 @@ def audio_to_patch_embeddings(weight, bias=None, patch_size=4, stride=None):
     return forward_pass
 
 def model_and_parameters(device):
-    node_runner, parameters, = universal_node(NUM_EMBED_DIM, NUM_MINI_POOL, NEURONS_PER_MINI_POOL, NUM_CLASSES, DROPOUT, REFINEMENT_STEPS, device)
+    # Model configs
+    REFINEMENT_STEPS = 3
+    NUM_MINI_POOL = 8
+    NEURONS_PER_MINI_POOL = 256
+    DROPOUT = 0.1
+    NUM_CLASSES = 10
+    AUDIO_PATCH_SIZE = 16
+
+    node_runner, parameters, = universal_node(NEURONS_PER_MINI_POOL, NUM_MINI_POOL, NEURONS_PER_MINI_POOL, DROPOUT, REFINEMENT_STEPS, False, device)
 
     # For vision parameters
-    parameters['conv2d_weights'] = torch.nn.Parameter(torch.zeros(NUM_EMBED_DIM, 1, 16, 16, device=device).normal_(std=0.02), requires_grad=False)
-    parameters['conv2d_bias'] = torch.nn.Parameter(torch.zeros(NUM_EMBED_DIM, device=device).normal_(std=0.02), requires_grad=False)
-    parameters['read_out'] = torch.nn.Parameter(torch.zeros((NUM_EMBED_DIM, NUM_CLASSES), device=device).normal_(std=0.02))
+    parameters['conv2d_weights'] = torch.nn.Parameter(torch.zeros(NEURONS_PER_MINI_POOL, 1, AUDIO_PATCH_SIZE, AUDIO_PATCH_SIZE, device=device).normal_(std=0.02), requires_grad=False)
+    parameters['conv2d_bias'] = torch.nn.Parameter(torch.zeros(NEURONS_PER_MINI_POOL, device=device).normal_(std=0.02), requires_grad=False)
+    parameters['read_out'] = torch.nn.Parameter(torch.zeros((NEURONS_PER_MINI_POOL, NUM_CLASSES), device=device).normal_(std=0.02), requires_grad=False)
 
     total_parameters = sum([params.view(-1).size(-1) for _, params in parameters.items()])
 
@@ -76,18 +70,24 @@ def iterate_over_loader(dataloader, model_runner, optimizer=None):
     return round(sum(result_per_batch) / len(result_per_batch), 3)
 
 def main():
+    # Training configs
+    BATCH_SIZE = 32
+    MAX_EPOCHS = 3000
+    LEARNING_RATE = 1e-3
+
     with open('./datasets/audio.pkl', 'rb') as f: ((train_x, train_y), (test_x, test_y)) = pickle.load(f, encoding='latin1')
 
     model_runner, model_parameters = model_and_parameters(device='cuda')
-    optimizer = torch.optim.AdamW(torch.nn.ParameterList(model_parameters.values()), lr=0.001)
+    optimizer = torch.optim.AdamW(torch.nn.ParameterList(model_parameters.values()), lr=LEARNING_RATE)
 
-    for epoch in range(1, 100+1):
+    for epoch in range(1, MAX_EPOCHS+1):
         train_loader = audio_get_batch(train_x, train_y, batch_size=BATCH_SIZE, shuffle=True)
         validation_loader = audio_get_batch(test_x, test_y, batch_size=BATCH_SIZE, shuffle=False)
 
         avg_loss = iterate_over_loader(train_loader, model_runner, optimizer)
         avg_accuracy = iterate_over_loader(validation_loader, model_runner)
 
+        save_checkpoint('audio-checkpoints', epoch, model_parameters, optimizer)
         print(f'EPOCH: {epoch} loss {avg_loss} accuracy {avg_accuracy}')
 
 main()
