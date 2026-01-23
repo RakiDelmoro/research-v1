531f4edaa7a2f51545dd9d0428836f2eb765dd75
Raki Delmoro <rakidelmoro@gmail.com>
2026-01-23 12:49:44 +0000

    update

--- atomvision.py
+++ atomvision.py
@@ -25,7 +25,7 @@ def model_and_parameters(device):
     NUM_CLASSES = 10
     PATCH_SIZE = 4
 
-    node_runner, parameters, = universal_node(NEURONS_PER_MINI_POOL, NUM_MINI_POOL, NEURONS_PER_MINI_POOL, DROPOUT, REFINEMENT_STEPS, False, device)
+    node_runner, parameters, = universal_node(NUM_MINI_POOL, NEURONS_PER_MINI_POOL, DROPOUT, REFINEMENT_STEPS, False, device)
 
     # For vision parameters
     parameters['conv2d_weights'] = torch.nn.Parameter(torch.zeros(NEURONS_PER_MINI_POOL, 1, PATCH_SIZE, PATCH_SIZE, device=device).normal_(std=0.02), requires_grad=False)
